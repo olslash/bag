24d85b29578e4977e8368968420ff4cfc212754d
Mitch Robb <olordhux+github@gmail.com>
2016-08-14 03:48:34 -0700

    remove data ns -- handling elsewhere

--- src/buyme_aggregation_backend/data.clj
+++
@@ -1,8 +0,0 @@
-(ns buyme-aggregation-backend.data
-  (:require [taoensso.timbre :refer [info]]
-            [clj-http.client :as client]))
-
-(defn fetch-http [source]
-  (let [url (:url_root source)]
-    (info "Fetching from" source)
-    (:body (client/get url))))
