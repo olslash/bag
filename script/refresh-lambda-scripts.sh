511948fcf6bdaa20c8470afd5304a93a22fe2809
Mitch Robb <olordhux+github@gmail.com>
2016-11-23 16:15:28 -0800

    add scripts to refresh lambda functions

---
+++ script/refresh-lambda-scripts.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source "lambda_management_config.conf"
+
+read -p "Delete and replace all lambda handlers with ${jar_path}? [yN] " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+delete_lambda_function() {
+  aws lambda delete-function --function-name $1
+}
+
+create_lambda_function() {
+    aws lambda create-function \
+        --function-name $1 \
+        --handler $2 \
+        --runtime $handler_runtime \
+        --memory $handler_memory \
+        --timeout $handler_timeout \
+        --role $handler_aws_role \
+        --zip-file fileb://$jar_path
+}
+
+for i in `seq 0 2 $((${#handlers[@]} - 1))`; do
+    name=${handlers[$i]}
+    handler=${handlers[$i + 1]}
+
+    delete_lambda_function $name
+    echo "deleted function: $name"
+    create_lambda_function $name $handler
+    echo "created function: $name with $handler"
+done
