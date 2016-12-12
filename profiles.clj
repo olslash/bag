02bc01c2a854eab7a20c8597133e52c3f01c5e8d
Mitch Robb <olordhux+github@gmail.com>
2016-12-12 01:39:11 -0800

    add other needed stuff to profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,12 +1,19 @@
-{:dev  {:env {:environment       "development"
-              :database-name     "postgres"
-              :database-password "pw"
-              :database-port     "5432"
-              :database-user     "postgres"
-              :database-subname  "//localhost:5432/postgres"
-              :log-level         "info"}}
+{:dev {:env {:environment           "development"
+             :database-name         "postgres"
+             :database-password     "pw"
+             :database-port         "5432"
+             :database-user         "postgres"
+             :database-subname      "//localhost:5432/postgres"
+             :imgur-client-id       "your-id"
+             :aws-lambda-exec-role  "arn:aws:iam::fixme:role/lambda-exec"
+             :aws-access-key-id     "your-access-key"
+             :aws-secret-access-key "your-secret-key"
+             :log-level             "info"}}}
 
- :test {:env {:environment      "production"
-              :database-port    "5432"
-              :database-subname "//localhost:5432/fixme"
-              :log-level        "debug"}}}
+:test {:env {:environment       "production"
+             :database-name     "postgres"
+             :database-password "pw"
+             :database-port     "5432"
+             :database-user     "postgres"
+             :database-subname  "//localhost:5432/postgres"
+             :log-level         "debug"}}
